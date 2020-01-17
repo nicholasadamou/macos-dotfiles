a492e5c4960f0648e30518123aa379c4695359ae
Nicholas Adamou <nicholasadamouemail@gmail.com>
2020-01-17 07:29:14 -0600

    updated bin/run

--- lib/options.sh
+++ lib/options.sh
@@ -8,15 +8,26 @@
 # $1 = The option to process.
 process_option() {
   case $1 in
-    's')
+		'-b|--no-base|--base|b|no-base')
+			_arg_base="on"
+
+			test "${1:0:5}" = "--no-" && _arg_base="off"
+			test "${1:0:5}" = "no-" && _arg_base="off"
+
+			# by default, run the base module.
+			if [[ ${_arg_base} == "on" ]]; then
+				bash "$DOTFILES_PATH"/base/base.sh
+			fi
+			;;
+    '-s|s')
       show_files;;
-		'c')
+		'-c|c')
 			check_files;;
-    'l')
+    '-l|l')
       symlink_files;;
-    'd')
+    '-d|d')
       delete_files;;
-    'q');;
+    '-q|q');;
     *)
       printf "%s\n" "ERROR: Invalid option.";;
   esac

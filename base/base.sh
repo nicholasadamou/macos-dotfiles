c04bc7ae1c4e761c0b1f5821de5f3e72c692fe40
Nicholas Adamou <nicholasadamouemail@gmail.com>
2020-04-05 06:51:00 -0400

    Update base.sh

--- base/base.sh
+++ base/base.sh
@@ -68,26 +68,15 @@ export PATH=\"/usr/local/sbin:\$PATH\"
     fi
 }
 
-change_default_bash() {
-    local configs=""
-    local pathConfig=""
+change_default_bash_version() {
 
-    local newShellPath=""
-    local brewPrefix=""
+    local PATH_TO_HOMEBREW_BASH=""
 
     # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-    # Try to get the path of the `Bash`
-    # version installed through `Homebrew`.
+    # Get the path of the `Bash` version installed through `Homebrew`.
 
-    brewPrefix="$(brew --prefix)"
-
-    pathConfig="PATH=\"$brewPrefix/bin:\$PATH\""
-    configs="# Homebrew bash configurations
-$pathConfig
-export PATH"
-
-    newShellPath="$brewPrefix/bin/bash"
+    PATH_TO_HOMEBREW_BASH="$(brew --prefix)/bin/bash"
 
     # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
@@ -101,52 +90,46 @@ export PATH"
     #
     # http://www.linuxfromscratch.org/blfs/view/7.4/postlfs/etcshells.html
 
-    if ! grep -q "$(<<<"$newShellPath" tr '\n' '\01')" < <(less "/etc/shells" | tr '\n' '\01'); then
-        printf '%s\n' "$newShellPath" | sudo tee -a /etc/shells
+    if ! grep -q "$(<<<"$PATH_TO_HOMEBREW_BASH" tr '\n' '\01')" < <(less "/etc/shells" | tr '\n' '\01'); then
+        printf '%s\n' "$PATH_TO_HOMEBREW_BASH" | sudo tee -a /etc/shells &> /dev/null
+    fi
+
+}
+
+change_default_shell_to_fish() {
+
+    local PATH_TO_FISH=""
+
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+    # Get the path of `fish` which was installed through `Homebrew`.
+
+    PATH_TO_FISH="$(brew --prefix)/bin/fish"
+
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+    # Add the path of the `fish` version installed through `Homebrew`
+    # to the list of login shells from the `/etc/shells` file.
+    #
+    # This needs to be done because applications use this file to
+    # determine whether a shell is valid (e.g.: `chsh` consults the
+    # `/etc/shells` to determine whether an unprivileged user may
+    # change the login shell for their own account).
+    #
+    # http://www.linuxfromscratch.org/blfs/view/7.4/postlfs/etcshells.html
+
+    if ! grep -q "$(<<<"$PATH_TO_FISH" tr '\n' '\01')" < <(less "/etc/shells" | tr '\n' '\01'); then
+        printf '%s\n' "$PATH_TO_FISH" | sudo tee -a /etc/shells &> /dev/null
     fi
 
     # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-    # Set latest version of `Bash` as the default
-    # (macOS uses by default an older version of `Bash`).
+    # Set latest version of `fish` as the default shell
 
-    if [ "$(dscl . -read /Users/"${USER}"/ UserShell | cut -d ' ' -f2)" != "${newShellPath}" ]; then
-        chsh -s "$newShellPath" &> /dev/null
+    if [[ "$(dscl . -read /Users/"${USER}"/ UserShell | cut -d ' ' -f2)" != "${PATH_TO_FISH}" ]]; then
+        chsh -s "$PATH_TO_FISH" &> /dev/null
     fi
 
-    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-    # If needed, add the necessary configs in the
-    # local shell configuration file.
-
-    if [ ! -e "$LOCAL_BASH_CONFIG_FILE" ] || ! grep -q "$(<<<"$configs" tr '\n' '\01')" < <(less "$LOCAL_BASH_CONFIG_FILE" | tr '\n' '\01'); then
-        printf '%s\n' "$configs" >> "$LOCAL_BASH_CONFIG_FILE" \
-            && . "$LOCAL_BASH_CONFIG_FILE"
-    fi
-}
-
-# see: https://github.com/oh-my-fish/oh-my-fish/issues/189
-install_omf() {
-    if ! is_omf_installed; then
-		# Make sure '$HOME/.local/share/omf' does not exist prior
-		# to 'omf' installation.
-
-		if [ -d "$HOME/.local/share/omf" ]; then
-			sudo rm -rf "$HOME/.local/share/omf"
-		fi
-
-		# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-        fish <(curl -Ls https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install) \
-				--noninteractive --yes --path="$HOME/.local/share/omf" --config="$HOME/.config/omf"
-    fi
-}
-
-install_omf_packages() {
-	omf_install "z"
-    omf_install "thefuck"
-
-    omf_update
 }
 
 install_fisher() {
@@ -182,8 +165,6 @@ main() {
 	change_default_bash
 
 	# Fish
-	install_omf
-    install_omf_packages
 	install_fisher
 	install_fisher_packages
 }
